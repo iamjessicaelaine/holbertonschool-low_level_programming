04c218d16b068a3d2dce4757997c1f2584adf65c
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-01-25 21:03:18 -0800

    response for task 7 in project 0x01

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main prints alphabet in lowercase followed by a new line
+ * Return: return 0 for success
+ */
+int main(void)
+{
+char x;
+for (x = 'z'; x <= 'a'; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
