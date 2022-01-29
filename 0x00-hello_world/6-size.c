646c2533f5721225404d57bb54a94ce03a2d1e89
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-01-29 11:35:11 -0800

    finally found the typo, prayerfully this is the final commit for project 0x00

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %c byte(s)\n", sizeof(char));
-printf("Size of an int: %d bytes(s)\n", sizeof(int));
-printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lld byte(s)\n", sizeof(long long));
-printf("Size of a float: %f byte(s)", sizeof(float));
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long));
+printf("Size of a float: %i byte(s)\n", sizeof(float));
 return (0);
 }
