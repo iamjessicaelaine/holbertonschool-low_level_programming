281c830f2701fb787e0fde07c069dcda80694a8d
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-02-01 17:24:40 -0800

    response for task 1 project 0x04

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit  -  checks if input is a digit
+ * @c: c is being tested if it is in a digit or not
+ * Return: return 0 for success
+*/
+
+int _isdigit(int c)
+{
+if (c >= 0)
+return (1);
+else
+return (0);
+}
