2ec4f7826c4964aa3f869e34530e24bab8d01ffd
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-02-07 14:30:45 -0800

    intitial response for taswk 2 project 0x06

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * strncpy - function that copies a string
+ * @dest: a string where src will be copied to
+ * @src: a string to be copied
+ * @n: the number of characters to copy
+ * Return: is dest string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+char *j = dest;
+
+while (*src && n--)
+{
+
+*j = *src;
+j++;
+src++;
+}
+return (dest);
+}
