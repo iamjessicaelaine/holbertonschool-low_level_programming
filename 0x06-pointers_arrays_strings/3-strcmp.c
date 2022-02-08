fcbd07aba2722cd95c600d2564c2ad104162aacd
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-02-07 20:16:48 -0800

    update to task 3 project 0x06

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,21 +4,22 @@
  * _strcmp - a function that compares two strings
  * @s1: the first string to be compared
  * @s2: the second string to be compared
- * Return: 0 for strings being same, return -1 for different strings
+ * Return: 0 for strings being same, return - number for different strings
  */
 
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 == *s2)
+while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 {
-if (*s1 == '\0' || *s2 == '\0')
-break;
-
 s1++;
 s2++;
 }
 if (*s1 == *s2)
+{
 return (0);
+}
 else
-return (-1);
+{
+return (*s1 - *s2);
+}
 }
