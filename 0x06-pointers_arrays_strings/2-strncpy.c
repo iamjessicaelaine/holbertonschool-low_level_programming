4047c2e5228bc7f7a37fb9671a53e29d9f19d497
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-02-07 18:17:47 -0800

    switching from while loop to for loop to correct output for task 2 project 0x06

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,11 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-char *j = dest;
+int j;
 
-while (*src && n++)
-{
-
-*j = *src;
-j++;
-src++;
-}
-
-return (j);
+for (j = 0; j < n && src[j] != '\0'; j++)
+dest[j] = src[j];
+for ( ; j < n; j++)
+dest[j] = '\0';
+return (dest);
 }
