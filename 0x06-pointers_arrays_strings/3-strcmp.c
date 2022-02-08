3c59f8e8d33fa1b914094923342709e5047f3638
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-02-07 19:42:47 -0800

    response for task 3 project 0x06

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: the first string to be compared
+ * @s2: the second string to be compared
+ * Return: 0 for strings being same, return -1 for different strings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 == *s2)
+{
+if (*s1 == '\0' || *s2 == '\0')
+break;
+
+s1++;
+s2++;
+}
+if (*s1 == '\0' && *s2 == '\0')
+return (0);
+else
+return (-1);
+}
