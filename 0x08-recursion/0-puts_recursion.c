9808bda77d7d4a906900166f7364da54dc813dbf
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-02-09 20:36:34 -0800

    response for task 3 project 0x08

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string followed by a new line
+ * Return: a string followed by a new line
+ */
+
+void _puts_recursion(char *s)
+{
