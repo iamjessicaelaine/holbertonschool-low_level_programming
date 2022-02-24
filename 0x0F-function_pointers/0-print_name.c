74816c56ac48fd0076a9396d910f685493254be8
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-02-23 18:44:37 -0800

    response for task 0 project 0x10

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: char pointer to address of name
+ * @f: function pointer to function f
+ * Return: void
+ */
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
