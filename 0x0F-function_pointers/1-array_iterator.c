37993bece76452b36738603b3f3c7334daa7a0a7
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-02-23 20:53:24 -0800

    response for task 1 project 0x10

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: points to an integer array to be func'd up
+ * @size: the size of the array
+ * @action: pointer to the function that will func up array's elements
+ * Return: void
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int j = 0; /* counter variable for array */
+
+	if (array && size && action) /* protects function's func from NULL */
+		while (j < size) /* loop to access ea. element so it's func'd */
+		{
+			j++;
+			action(array[j]);
+		}
+}
