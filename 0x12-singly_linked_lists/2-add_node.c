b1ad62a4856cb749e0b0d7bddaaf98dc8703dd83
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-03-08 02:36:10 -0800

    include if string == null for error task 2 project 0x12

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *string)
 	list_t *new_node;
 
 	new_node = (list_t *) malloc(sizeof(list_t));
-	if (new_node == NULL)
+	if (new_node == NULL || string == NULL)
 	{
 		return (NULL);
 	}
