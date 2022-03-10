c3e04f82e83c57c52729b2b5d4e1770c53e7a1e8
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-03-10 01:41:18 -0800

    update to code for function #7 to account for empty list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current = head;
 	unsigned int count = 0;
 
-	if (!index)
+	if (!index || head == NULL)
 	{
 		return (NULL);
 	}
