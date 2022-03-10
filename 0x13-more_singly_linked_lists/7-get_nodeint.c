a54d8ba1b06059f9b032a1a2b570d9e68f0b1709
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-03-10 15:27:37 -0800

    code finally works had to get betty together tho :)

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -20,10 +20,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		if (count == index)
 		{
 			retrieved = current;
-			return(retrieved);
+			return (retrieved);
 		}
 		count++;
 		current = current->next;
 	}
-	return(NULL);
+	return (NULL);
 }
