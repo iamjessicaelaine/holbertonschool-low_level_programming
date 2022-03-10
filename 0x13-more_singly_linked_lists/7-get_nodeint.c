c015daca79e1da8ac98e4ff4db3b13e97eb8de58
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-03-10 13:37:26 -0800

    code update for function #7 tightened up while loop condition

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		return (NULL);
 	}
-	while (current != NULL)
+	while (current != NULL && count < index)
 	{
 		count++;
 		current = current->next;
