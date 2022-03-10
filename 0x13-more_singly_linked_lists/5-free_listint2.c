0761a5bdf9539bddf5c7b5630e9e0d6bc6c2d9c2
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-03-09 20:56:20 -0800

    code for function #5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list and sets the head to NULL
+ * which essentially deletes the entire linked list
+ * @head: points to the pointer of the listint_t list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	/* deref head to get the real head */
+	listint_t *freedom = *head;
+	listint_t *next = NULL;
+
+	while (freedom != NULL)
+	{
+		next = freedom->next;
+		free(freedom);
+		freedom = next;
+	}
+	/* deref head to affect the real head */
+	*head = NULL;
+}
