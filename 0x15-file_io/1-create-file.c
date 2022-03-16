d1ff096bc4a11a074ce12338ca388c13261cb2df
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-03-15 21:52:39 -0700

    update to file for checker points

--- 0x15-file_io/1-create-file.c
+++ 0x15-file_io/1-create-file.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
+ * create_file - reads a text file and prints it to the POSIX standard output
  * @filename: points to the text file to be read
- * @letters: the number of letters it should read and print
+ * @text_content: the number of letters it should read and print
  * Return: the actual number of letters it could read and print or returns 0 if
  * the file cannot be opened or read, if the filename is NULL, or if write fails
  * or does not write the expected amount of bytes.
  */
 
-ssize_t read_textfile(const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content)
 {
 	int filedescriptor; /* return value of open */
 	ssize_t actualfactual; /* actual number of letters read & printed */
