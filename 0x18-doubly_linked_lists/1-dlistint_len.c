b0bb8c0a1b6e5f5b1ff5b598929d134dd3cee72c
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-04-04 09:58:38 -0700

    code for function 1

---
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_lenn - returns the number of elements in doubly linked list
+ * @head: pointer to doubly linked list
+ * Return: # number of elements in doubly linked list
+ */
+
+size_t dlistint_len(const dlistint_t *head)
+{
+	unsigned int nodecount = 0;
+	/* declare traversing pointer & set it to start of list */
+	const dlistint_t *current = head;
+
+	/* start loop that will traverse through list until the end */
+	while (current != NULL)
+	{
+		nodecount++;
+		current = current->next;
+	}
+	return (nodecount);
+}
